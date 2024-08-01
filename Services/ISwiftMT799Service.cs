b54bab0749e4e6a286998476835da3839fa87627
Kaloyan <kaloyanpavlov4@gmail.com>
2024-08-01 12:54:10 +0300

    Add mt799 model, controller and interfaces for service and repository

---
+++ Services/ISwiftMT799Service.cs
@@ -0,0 +1,12 @@
+﻿
+using swiftmt799_web_api.Models;
+
+namespace swiftmt799_web_api.Services
+{
+    public interface ISwiftMT799Service
+    {
+        Task<IEnumerable<SwiftMT799Message>> FindAllAsync();
+
+        Task SaveAsync(IFormFile file);
+    }
+}
