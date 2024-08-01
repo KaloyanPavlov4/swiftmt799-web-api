12d29714d000984e0a3fbb3f8d7bea095179ca93
Kaloyan <kaloyanpavlov4@gmail.com>
2024-08-01 12:34:40 +0300

    Add default connection and DB initialization method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.Data.Sqlite;
+
 namespace swiftmt799_web_api
 {
     public class Program
@@ -10,6 +12,8 @@ namespace swiftmt799_web_api
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            initDB(builder.Configuration.GetValue<string>("ConnectionStrings:DefaultConnection"));
 
             var app = builder.Build();
 
@@ -24,6 +28,26 @@ namespace swiftmt799_web_api
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static void initDB(string dataSource)
+        {
+            using (var connection = new SqliteConnection(dataSource))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = @"CREATE TABLE IF NOT EXISTS swift_mt799_messages (
+                                        basicHeader VARCHAR(255),
+                                        applicationHeader VARCHAR(100),
+                                        userHeader VARCHAR(16),
+                                        transactionReference VARCHAR(16) PRIMARY KEY,
+                                        relatedReference VARCHAR(16),
+                                        narrative VARCHAR(7000),
+                                        trailer varchar(255)
+                                        );";
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
